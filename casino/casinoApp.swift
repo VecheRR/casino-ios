ffd49bc7725f3061abc0bb644c27a043b94ad3fb
vecher <vecherkovsky2003@mail.ru>
2022-06-10 19:06:26 +0300

    Initial Commit

---
+++ casino/casinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  casinoApp.swift
+//  casino
+//
+//  Created by Владичка Вечер on 10.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct casinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
